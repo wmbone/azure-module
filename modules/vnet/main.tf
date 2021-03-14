10b0912a337fb1952663baa624b86d3a0ab39c69
wmbone <24934694+wmbone@users.noreply.github.com>
2021-03-14 22:12:25 +1100

    change cidrsubnet prefix

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -2,14 +2,14 @@
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "dev" {
   name                = "${var.name}_azure_vnet"
-  address_space       = ["192.168.10.0/20"]
+  address_space       = [var.address_space[0]]
   location            = var.location
   resource_group_name = var.resource_group_name
   count = var.subnet_count > 0? var.subnet_count:0
   subnet {
     //count = var.subnet_count > 0? var.subnet_count:0
     name = "subnet${count.index}"
-    address_prefix = "cidrsubnet(var.address_space,16?,count.index?)"
+    address_prefix = cidrsubnet(var.address_space[0],4,count.index)
 
   }
 
